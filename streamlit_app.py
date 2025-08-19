3d7ccf914f6cede95fbe793b86127182f0016f3d
Akhilesh singh <akhi.singh1989@gmail.com>
2025-08-19 11:30:55 +1000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,7 @@
 import streamlit as st
 from scripts.transform import load_and_transform
 from dashboards.engineer_view import engineer_dashboard
+from dashboards.management_view import management_dashboard
 
 # File paths
 qualys_file = "data/raw/qualys_scan.csv"
@@ -10,6 +11,12 @@ lifecycle_file = "data/raw/lifecycle.csv"
 # Load & merge data
 df = load_and_transform(qualys_file, patch_file, lifecycle_file)
 
-# Engineer Dashboard
-engineer_dashboard(df)
+# Sidebar for navigation
+st.sidebar.title("Dashboard Navigation")
+page = st.sidebar.selectbox("Select Page", ["Management View", "Engineer View"])
+
+if page == "Management View":
+    management_dashboard(df)
+else:
+    engineer_dashboard(df)
 
