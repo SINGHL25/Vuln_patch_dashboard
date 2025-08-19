dd546ef93dbe792e1850e5a3f2464af43feda808
Akhilesh singh <akhi.singh1989@gmail.com>
2025-08-19 11:28:25 +1000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,27 +1,15 @@
 import streamlit as st
-from dashboards import engineer_view, management_view
 from scripts.transform import load_and_transform
+from dashboards.engineer_view import engineer_dashboard
 
-
-
-# Provide CSV file paths
+# File paths
 qualys_file = "data/raw/qualys_scan.csv"
 patch_file = "data/raw/patch_report.csv"
 lifecycle_file = "data/raw/lifecycle.csv"
 
-# Call function with arguments
+# Load & merge data
 df = load_and_transform(qualys_file, patch_file, lifecycle_file)
 
-
-# ETL: unify raw data
-load_and_transform()
-
-# Sidebar
-st.sidebar.title("Dashboard Selector")
-view = st.sidebar.radio("Choose View", ["Engineer", "Management"])
-
-if view == "Engineer":
-    engineer_view.engineer_dashboard()
-else:
-    management_view.management_dashboard()
+# Engineer Dashboard
+engineer_dashboard(df)
 
