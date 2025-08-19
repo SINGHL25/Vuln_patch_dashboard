9bf507ee469a631729e7ed6446df7f0eff1d61b3
Akhilesh singh <akhi.singh1989@gmail.com>
2025-08-19 11:11:33 +1000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,1 +1,16 @@
+import streamlit as st
+from dashboards import engineer_view, management_view
+from scripts.transform import load_and_transform
+
+# ETL: unify raw data
+load_and_transform()
+
+# Sidebar
+st.sidebar.title("Dashboard Selector")
+view = st.sidebar.radio("Choose View", ["Engineer", "Management"])
+
+if view == "Engineer":
+    engineer_view.engineer_dashboard()
+else:
+    management_view.management_dashboard()
 
