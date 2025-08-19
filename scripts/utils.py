b5d4ca0eb2fff5e353cb93fb4384a1a48cea748d
Akhilesh singh <akhi.singh1989@gmail.com>
2025-08-19 11:10:51 +1000

    Update utils.py

--- scripts/utils.py
+++ scripts/utils.py
@@ -1,1 +1,8 @@
+import pandas as pd
+
+def load_unified_data():
+    return pd.read_csv("data/processed/unified_data.csv")
+
+def server_summary(df, server_id):
+    return df[df['ServerID'] == server_id].T
 
