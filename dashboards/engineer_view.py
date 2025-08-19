40deee2d02e9bd988719292ba3c8c4fcacf0423e
Akhilesh singh <akhi.singh1989@gmail.com>
2025-08-19 11:28:11 +1000

    Update engineer_view.py

--- dashboards/engineer_view.py
+++ dashboards/engineer_view.py
@@ -1,30 +1,14 @@
 
 import streamlit as st
-import pandas as pd
-import plotly.express as px
-from scripts.utils import load_unified_data
 
-
-
-
-def engineer_dashboard():
+def engineer_dashboard(df):
     st.title("🛠 Engineer Dashboard - Server Drilldown")
-    st.table(server_data[['ServerName','IP','OS_Version','Lifecycle','Compliance']])
 
-    
-    df = load_unified_data()
     servers = df['ServerID'].unique()
-    
     selected_server = st.selectbox("Select Server", servers)
-    server_data = df[df['ServerID']==selected_server]
-    
+
+    server_data = df[df['ServerID'] == selected_server]
+
     st.subheader(f"Details for Server: {selected_server}")
-    st.table(server_data[['ServerName','IP','OS_Version','Lifecycle','Compliance']])
-    
-    st.subheader("Vulnerabilities by Severity")
-    fig = px.bar(server_data, x='Vulnerability', y='CVSS', color='Severity', title='Vulnerabilities')
-    st.plotly_chart(fig)
-    
-    st.subheader("Missing Patches")
-    fig2 = px.pie(server_data, names='Patch', values='Status', title='Patch Status')
-    st.plotly_chart(fig2)
+    st.table(server_data[['ServerName','IP','OS_Version','Lifecycle','Compliance','Vulnerabilities']])
+
