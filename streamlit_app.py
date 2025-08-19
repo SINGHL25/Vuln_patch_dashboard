17e1f83fbc1357ac58345cc13332657ab780556b
Akhilesh singh <akhi.singh1989@gmail.com>
2025-08-19 11:26:40 +1000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,17 @@ import streamlit as st
 from dashboards import engineer_view, management_view
 from scripts.transform import load_and_transform
 
+
+
+# Provide CSV file paths
+qualys_file = "data/raw/qualys_scan.csv"
+patch_file = "data/raw/patch_report.csv"
+lifecycle_file = "data/raw/lifecycle.csv"
+
+# Call function with arguments
+df = load_and_transform(qualys_file, patch_file, lifecycle_file)
+
+
 # ETL: unify raw data
 load_and_transform()
 
